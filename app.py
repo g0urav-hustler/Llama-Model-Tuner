896f08efd28d094501c758d50ac40a3f8bdb457d
g0urav-hustler <gouravchouhan966@gmail.com>
2024-06-04 17:56:13 +0530

    update app file

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ import yaml
 import shutil
 import streamlit as st
 import pandas as pd
+from pathlib import Path
+from src.utils.common import read_yaml
 from src.components.data_ingestion import DataIngestion
 from src.components.data_processing import DataProcessing
 from src.components.train_model import ModelTrain
 from transformers import AutoTokenizer, AutoModelForCausalLM , pipeline
-
-
 
 
 @st.cache_resource(show_spinner="Loading model tokenizer...")
@@ -24,9 +24,7 @@ def load_model( model_path):
     return model
 
 def compressing_model(model_path, zip_file_name):
-    
     shutil.make_archive(zip_file_name, "zip", model_path)
-
 
 training_result = None
 
@@ -36,9 +34,6 @@ st.set_page_config(page_title="LLama Model Tuner", layout="wide")
 
 # title
 st.title("LLama Model Tuner Application")
-
-#subtitle
-st.markdown("This application helps you to fine llama model on your custom dataset. ")
 
 uploaded_file = st.file_uploader("Upload your data set file in csv format")
 
@@ -65,7 +60,7 @@ if uploaded_file is not None:
             answer_column = st.selectbox(label = "Select the answer column", options= df_columns)
 
         train_data_range = list(range(10,100,10))
-        data_size_col1, data_size_col2, data_size_col3 = st.columns(3)
+        data_size_col1, data_size_col2 = st.columns(2)
 
         # train_data_size input
         # train_data_range = 80 
@@ -78,16 +73,14 @@ if uploaded_file is not None:
             val_data_size = st.selectbox("Enter validation data size ", options= validation_data_range)
 
         test_data_range = list(range(0,100,10))
-        with data_size_col3:
-            test_data_size = st.selectbox("Enter test data size ", options= test_data_range)
-
+        
         st.markdown("Select the model parameter")
         
         par_col1, par_col2, par_col3, par_col4 = st.columns(4)
 
         # model type input
         with par_col1:
-            model_name = st.text_input(label="Enter model name ", value = None)
+            model_name = st.text_input(label="Enter model name ", value = "")
 
         # epochs input
         with par_col2:
@@ -144,32 +137,32 @@ if uploaded_file is not None:
             train_button = st.button("Train The Model")
         if train_button and not submitted:
             with st.status("Training Status", expanded=True) as status:
-                st.write("Data Loading.")
+                st.write("Data Ingestion Stage")
                 data_ingestion_process = DataIngestion()
                 data_ingestion_process.get_raw_data()
                 time.sleep(3)
-                st.write("Data Processing")
+                st.write("Data Processing Stage")
                 data_processing_process = DataProcessing()
                 data_processing_process.get_processed_data()
                 data_processing_process.get_split_data()
                 time.sleep(2)
-                st.write("Training model..")
-                # train_model_process = ModelTrain()
-                # training_result = train_model_process.train_model()
-                # shutil.copytree("/home/gourav/ML/QA_Models_Builder/logs/bert", os.path.join("artifacts","models", model_type))
-                # compressing_model(os.path.join("artifacts","models",model_name), model_name)
+                st.write("Training Model Stage")
+                train_model_process = ModelTrain()
+                training_result = train_model_process.train_model()
+                training_result = {"accuracy" : 0.98732, "loss": 0.88233}
+                compressing_model(os.path.join("artifacts","models",model_name), model_name)
                 status.update(label="Model Trained Succesfully", state="complete", expanded=False)
  
             st.success('Done!')
 
     if os.path.isdir(os.path.join("artifacts","models",model_name)):
-
         if training_result != None:
 
             result_col1, result_col2 = st.columns(2)
                 
             with result_col1:
                 st.write("Model Parameters")
+                params_data = read_yaml(Path("params.yaml"))
                 st.write(params_data["model_params"])
 
             with result_col2:
@@ -183,7 +176,6 @@ if uploaded_file is not None:
 
         input_prompt = st.text_input("Try any question.. ",None, placeholder= "Write question here..")
         if input_prompt:
-            st.write("Prediction")
             pipe = pipeline(task="text-generation", model= model, tokenizer= tokenizer, max_length=200)
             result = pipe(f"[INST] {input_prompt} [/INST]")
             print(result[0]['generated_text'])
@@ -191,7 +183,7 @@ if uploaded_file is not None:
             st.write("Predicted Answer")
             st.write(answer)
 
-        with open(f"{model_name}.zip", "rb") as fp:
+        with open(f"bert.zip", "rb") as fp:
             btn = st.download_button(
                 label="Download Trained Model",
                 data=fp,
